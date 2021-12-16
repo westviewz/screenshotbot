615cf4fcd8b3142041efc9af9b52cbcfa21a40b5
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-16 19:38:29 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -6,28 +6,29 @@ from bot.config import Config
 
 
 BUTTONS = [[
-    InlineKeyboardButton('Home 🏡', callback_data='home'),
-    InlineKeyboardButton('Close 📛', callback_data='close')
+    InlineKeyboardButton('🏡 Home', callback_data='home'),
+    InlineKeyboardButton('🚸 Powered By', callback_data='t.me/MoviesFlixers_DL'),
+    InlineKeyboardButton('🔐 Close ', callback_data='close')
 ]]
 
 HELP_TEXT = """
-Hi {mention}. Welcome to Screenshot Generator Bot. You can use me to generate:
+Hey! {mention}. Welcome to Screenshot Generator Bot. You can use me to generate:
 
     1. Screenshots.
     2. Sample Video.
     3. Trim Video.
 
-👉 I support any kind of **telegram video file** (streaming video or document video files) provided it --has proper mime-type-- and --is not corrupted--.
-👉 I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
-Just send me the telegram file or the streaming URL.
+☞ I support any kind of **telegram video file** (streaming video or document video files) provided it --has proper mime-type-- and --is not corrupted--.
+☞ I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
+☞ Just send me the telegram file or the streaming URL.
 
 See /settings to configure bot's behavior.
 Use /set_watermark to set custom watermarks to your screenshots.
 
 **General FAQ.**
 
-👉 If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--. Then make sure the file is a **video file** which satisfies above mentioned conditions.
-👉 If bot replies __😟 Sorry! I cannot open the file.__, the file might be --currupted-- or --is malformatted--.
+☞ If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--. Then make sure the file is a **video file** which satisfies above mentioned conditions.
+☞ If bot replies __😟 Sorry! I cannot open the file.__, the file might be --currupted-- or --is malformatted--.
 
 __If issues persists contact my father.__
 
