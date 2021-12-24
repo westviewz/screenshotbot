b1ffe1519eddfe82d0b95a9ffe5875a3e1befa3e
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-24 10:16:32 +0530

    Update forcesub.py

--- bot/database/forcesub.py
+++ bot/database/forcesub.py
@@ -1,7 +1,7 @@
 # (c) @PredatorHackerzZ
 
 import asyncio
-from sample_config import Config
+from bot.config import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
